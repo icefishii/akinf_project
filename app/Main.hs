9409a4d5b5a8d1ac35b9464dd8024f4576e9b45a
Benjamin Lampart <sera@icefish.dev>
2025-07-02 22:58:49 +0200

    start

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import Data.Csv (decodeByName, FromNamedRecord, (.:))
+import qualified Data.Csv as Csv
+import Control.Monad (forM_)
+import GHC.Generics (Generic)
+
+data Stock = Stock
+    { date   :: !String
+    , open   :: !Double
+    , high   :: !Double
+    , low    :: !Double
+    , close  :: !Double
+    , volume :: !Int
+    , name   :: !String
+    } deriving (Show, Generic)
+
+instance FromNamedRecord Stock where
+    parseNamedRecord m = Stock
+        <$> m .: "date"
+        <*> m .: "open"
+        <*> m .: "high"
+        <*> m .: "low"
+        <*> m .: "close"
+        <*> m .: "volume"
+        <*> m .: "Name"
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    csvData <- BL.readFile "all_stocks_5yr.csv"
+    case decodeByName csvData of
+        Left err -> putStrLn ("CSV parse error: " ++ err)
+        Right (_, v) ->
+            forM_ (V.take 5 (v :: V.Vector Stock)) print
